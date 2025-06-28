ce1594eff8d69748eff7bfdf5b649d96ab58af71
nrohitydv <nrohityadavv@gmail.com>
2025-06-28 23:56:55 +0545

    chore: update dependencies and add Navbar component
    - Updated package.json to include new dependencies:
      - @auth/prisma-adapter
      - @dnd-kit/core
      - @dnd-kit/sortable
      - @prisma/client
      - @radix-ui/react-tabs
      - @react-google-maps/api
      - @uploadthing/react
      - class-variance-authority
      - clsx
      - lucide-react
      - next-auth
      - react-globe.gl
      - tailwind-merge
      - tw-animate-css
      - uploadthing
    - Updated devDependencies:
      - Added @types/three
      - Updated prisma and eslint-config-next versions
    - Created Navbar component with basic structure

---
+++ components/Navbar.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav className="bg-white shadow-md py-4 border-b border-gray-200">
+      Hello
+    </nav>
+  );
+};
+
+export default Navbar;
