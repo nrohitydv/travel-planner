f94f687bb354e6e8f5ba9c077b476f713f4aee0f
nrohitydv <nrohityadavv@gmail.com>
2025-07-01 23:17:02 +0545

    implement authentication with NextAuth and GitHub provider, add user and session models

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,3 +1,5 @@
+"use client";
+import { login } from "@/lib/auth-actions";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
@@ -19,7 +21,10 @@ const Navbar = () => {
           <Link href={"/globe"} className="text-slate-900 hover:text-sky-500">
             Globe
           </Link>
-          <button className="flex items-center justify-center bg-gray-800 hover:bg-gray-900 text-white p-2 rounded-sm cursor-pointer ">
+          <button
+            className="flex items-center justify-center bg-gray-800 hover:bg-gray-900 text-white p-2 rounded-sm cursor-pointer "
+            onClick={login}
+          >
             Sign In
             <svg
               xmlns="http://www.w3.org/2000/svg"
